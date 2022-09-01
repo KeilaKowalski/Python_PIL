540eedae02052a2063ef679a03a31b44f1e80b23
Keila10 <keilakowalski10@gmail.com>
2022-09-01 19:36:53 -0300

    entradas y salidas de datos

---
+++ clase_1/entrada_salida_datos.py
@@ -0,0 +1,23 @@
+# Salida de datos
+print("hola mundo")
+
+# Otras funciones del print
+print(2+2)
+
+# Salto de linea
+print("Esto es un salto \n de linea")
+
+#Tabulacion
+print("Con esto se \t tabula")
+
+#Concatenar
+print("Hola " + "mundo..")
+
+# Sep
+print("hola", "mundo", sep="+")
+
+# End - quita salto de linea al finalizar el codigo
+print("hola", "mundo", end="")
+print("desde visual")
+
+# Entrada de datos
