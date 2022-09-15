4c2cd4f9fcb4a2b7ce9179512defa05bf7b4c3c3
Keila10 <keilakowalski10@gmail.com>
2022-09-15 20:08:44 -0300

    clase 5 POO

---
+++ clase_5/poo.py
@@ -0,0 +1,61 @@
+# Clase
+class Animal:
+    def __init__(self, especie, edad):
+        self.especie = especie
+        self.edad = edad
+    
+    def hablar(self, sonido):
+        print(sonido)
+    
+
+
+class Perro(Animal):
+     # Atributos de clase == variable global
+    especie = "mamiferos"
+
+    def __init__(self, nombre, raza, especie, edad):
+        # Atributos de instancia == variables locales
+        self.nombre = nombre
+        self.raza = raza
+
+        super().__init__(especie, edad)
+
+      # Metodos
+    # def jugar(self, objeto):
+    #     print("El perro esta jugando con ", objeto)
+
+    def saludar(self):
+        print(f'{self.nombre} dio la pata')
+
+class Gato(Animal):
+     # Atributos de clase == variable global
+    especie = "mamiferos"
+
+    def __init__(self, nombre, raza, especie, edad):
+        # Atributos de instancia == variables locales
+        self.nombre = nombre
+        self.raza = raza
+
+        super().__init__(especie, edad)
+
+      # Metodos
+    # def jugar(self, objeto):
+    #     print("El perro esta jugando con ", objeto)
+
+    def saludar(self):
+        print(f'{self.nombre} ronronea')
+
+
+perro1 = Perro("Firulais", "Salchicha", "Perro", 5)
+print(f'Perro1 -> {perro1.nombre}, {perro1.raza}, {perro1.especie}')
+perro1.saludar()
+
+gato1 = Gato("Tito", "Calico", "Felino", 3)
+print(f'Gato1 -> {gato1.nombre}, {gato1.raza}, {gato1.especie}, {gato1.edad}')
+gato1.saludar()
+
+
+
+
+
+
